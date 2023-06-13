6d28c5b71234cba151def59259816978d9a645fc
socia <socianudaniel@gmail.com>
2023-06-13 13:21:14 +0200

    add user to core and migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 # Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email','name')
+
+admin.site.unregister(Group)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+"""Models for the smdbookings project"""
 
-# Create your models here.
+
+from django.db import models
+from django.contrib.auth.base_user import (
+    AbstractBaseUser,
+    BaseUserManager
+)
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+class UserManager(BaseUserManager):
+    """Manager for the  custom user model"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create user with given email and password"""
+        if not email:
+            raise ValueError(_('User must have an email'))
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    def create_superuser(self, email, password):
+        """Create a superuser with email and password"""
+        user = self.create_user(email=email, password=password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User system for the project email as username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    objects= UserManager()
+
+    USERNAME_FIELD = 'email'
+
