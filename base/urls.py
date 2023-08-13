ab10755e097244312b162b8d8ee01b6acd333fbd
Smdonline <4cVVgJN5dZYUhtM>
2023-08-13 12:12:32 +0200

    add location model

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
-from location import urls as location_urls
+
 
 app_name = 'base'
 
 urlpatterns = [
     path('', views.index, name="main"),
-    path('location/',include('location_urls'))
+    path('location/', include('location.urls'), name='location')
 ]
--- location/urls.py
+++ location/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.views.generic import ListView, CreateView, DetailView
 from core import models
 app_name = 'location'
-
 urlpatterns = [
-    path('', ListView.as_view(template_name='location/list', model=models.Location)),
+    path('', ListView.as_view(template_name='location/list.html', model=models.Location)),
 ]
