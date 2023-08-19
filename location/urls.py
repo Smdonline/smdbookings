f8f359288e9984f28af320041f7c48450727eb6d
Smdonline <4cVVgJN5dZYUhtM>
2023-08-19 15:46:08 +0200

    add method for add view

--- location/urls.py
+++ location/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.views.generic import ListView, CreateView, DetailView
 from core import models
+from . import views
 app_name = 'location'
 urlpatterns = [
-    path('', ListView.as_view(template_name='location/list.html', model=models.Location)),
+    path('', ListView.as_view(template_name='location/list.html', model=models.Location), name='list'),
+    path('add/', views.AddLocationView.as_view(), name='add'),
+    # path('edit/<slug:loc_slug>/', views.edit_location, name='edit'),
+    # path('delete/<slug:loc_slug>/', views.delete_location, name='delete'),
 ]
--- location/views.py
+++ location/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+import core.models
+
 
 # Create your views here.
+class AddLocationView(CreateView):
+    template_name = 'location/add.html'
+    model = core.models.Location
+    fields = '__all__'
