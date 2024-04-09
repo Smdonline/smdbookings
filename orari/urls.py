93a0730bf2ca265def703a9fd920068d55ed4507
socia <socianudaniel@gmail.com>
2024-04-09 23:06:22 +0200

    add delete view to orario

--- orari/urls.py
+++ orari/urls.py
@@ -3,9 +3,10 @@ from . import views
 app_name='orari'
 urlpatterns = [
     path('update/<int:pk>/', views.UpdateOrari.as_view(), name='update'),
+    path('delete/<int:pk>/', views.DeleteOrario.as_view(), name='delete'),
     path('<slug:slug>/', views.CreateOrarioView.as_view(), name="add_orario"),
     path('<slug:slug>/dash/', views.ListOrariForEdit.as_view(), name="dash_orario"),
     path('<slug:slug>/<int:weekday>/', views.CreateOrarioView.as_view(), name="add_orario"),
-    path('delete/<int:pk>/', views.DeleteOrario.as_view(), name='delete'),
+
 
 ]
